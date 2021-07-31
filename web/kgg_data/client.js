f789ffbea1a61b66037aa00c4b2f3fc3bcaced44
konalt <79701511+konalt@users.noreply.github.com>
2021-07-31 14:57:35 +0100

    make getRandomSplash function synchronous deprecations?? whaaaaaaaa

--- web/kgg_data/client.js
+++ web/kgg_data/client.js
@@ -6,19 +6,25 @@ socket.on("connection_ack", () => {
 });
 
 // This function is for the future, when we'll have splash text
+// Yes, this is deprecated. No, I don't care.
 function getRandomSplash() {
+    // Have to do this for some reason
+    var toReturn = "Splash text error 0";
     $.ajax({
         type: "GET",
         url: "kgg_data/splashes.json",
+        async: false,
         dataType: "json",
         success: function(response) {
             console.log("Got splash texts epicly");
             // Choose random one and return it.
             var splash = response.splashes[Math.floor(Math.random() * response.splashes.length)];
-            return splash;
+            toReturn = splash;
         },
         error: function() {
             console.log("Couldn't get splash texts?????");
+            toReturn = "Splash text error 1";
         }
     });
+    return toReturn;
 }
